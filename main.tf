a9c85c249f024afdc24f0b141527ac52d212ef3c
Sanjaykumar Beerelli <sanjaykumarbeerelli@denb0844.speedport.ip>
2022-11-09 16:29:54 +0100

    Added new codebuild and updated path to Lambda function

--- main.tf
+++ main.tf
@@ -54,12 +54,12 @@ resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
 
 data "archive_file" "zip_the_python_code" {
     type = "zip"
-    source_dir = "${path.module}/python/"
-    output_path = "${path.module}/python/index.zip"
+    source_dir = "${path.module}/codebuild/python/"
+    output_path = "${path.module}/codebuild/python/lambda_function.zip"
 }
 
 resource "aws_lambda_function" "terraform_lambda_function" {
-    filename                       = "${path.module}/python/index.zip"
+    filename                       = "${path.module}/codebuild/python/lambda_function.zip"
     function_name                  = "lambda_function_from_tf"
     role                           = aws_iam_role.lambda_role.arn
     handler                        = "index.lambda_handler"

424949b97242e492cd19cef8acbfa4c1d7c150b7
Sanjaykumar Beerelli <sanjaykumarbeerelli@denb0844.speedport.ip>
2022-11-09 16:18:25 +0100

    Added Codebuild spec file

--- main.tf
+++ main.tf
@@ -46,7 +46,6 @@ EOF
 }
 
 
-
 resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
     role = aws_iam_role.lambda_role.name
     policy_arn = aws_iam_policy.iam_policy_for_lambda.arn
